daf778cf564f2fc553f666eef6cae111c10fa9e1
LuigiCarlBrezuela <luigi.brezuela@sorsu.edu.ph>
2025-04-30 03:02:44 +0800

    add new features authentication modals and carousel

---
+++ nextjs-app/app/clientportal/page.tsx
@@ -0,0 +1,16 @@
+import Feature from "@/components/carousel";
+import { Main } from "@/components/craft";
+import { Breadcrumbs } from "@/components/breadcrumbs";
+
+
+export default function Page() {
+    return (
+
+        <Main>
+            <Breadcrumbs />
+
+            <Feature />
+        </Main>
+
+    )
+}
