68236cccd5d2aee114a00c77835ef79ae49a2205
LuigiCarlBrezuela <luigi.brezuela@sorsu.edu.ph>
2025-05-25 14:20:03 +0800

    qwe

--- nextjs-app/next.config.mjs
+++ nextjs-app/next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://laravel-backend-4.onrender.com/:path*',
+        destination: 'https://laravel-backend-4.onrender.com/api/:path*',
       },
     ];
   },
